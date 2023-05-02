82cfb0e8a4a7f11d120aea5e35edf3bdf717eae8
FletchDavid <123987462+FletchDavid@users.noreply.github.com>
2023-05-02 17:51:34 -0500

    Started on a multi pic function
    Not fully working yet

--- main.py
+++ main.py
@@ -2,7 +2,19 @@ import cv2
 import numpy as np
 import PanoramaStitch as beet
 
-left_img = cv2.imread("left.jpg")
-right_img = cv2.imread("right.jpg")
+in_img = []
 
-panoramic = beet.stitch(left_img, right_img)
+NUM_IMG = 14
+
+for i in range(1, NUM_IMG + 1):
+    in_img.append(cv2.imread("SampleSets\library\\" + repr(i) + ".jpg"))
+
+panoramic = beet.stitch(in_img[0], in_img[1])
+
+for i in range(2, NUM_IMG):
+    temp = beet.stitch(panoramic, in_img[i])
+    panoramic.shape = temp.shape
+    panoramic = temp
+
+cv2.imsave("Panorama.png", panoramic)
+
